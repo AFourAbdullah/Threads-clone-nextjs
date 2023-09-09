30e2e961517c3713f7d9c09244ee53c58bb57046
Abdullah Aziz <abdullahazees4@gmail.com>
2023-09-10 02:07:38 +0500

    files added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,9 @@
+import { fetchThreads } from "@/lib/actions/threads.actions";
 import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const result = await fetchThreads(1, 30);
+  console.log("threadss:", result);
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
--- lib/actions/threads.actions.ts
+++ lib/actions/threads.actions.ts
@@ -1,7 +1,8 @@
 "use server";
+import { connectToDB } from "../mongoose";
 import Thread from "../models/threadmodel";
 import User from "../models/usermodel";
-import { connectToDB } from "../mongoose";
+// import { connectToDB } from "../mongoose";
 import { revalidatePath } from "next/cache";
 
 interface Params {
@@ -36,4 +37,40 @@ export async function createThread({
     throw new Error(`Failed to create thread because : ${error.message}`);
   }
   // cons
+}
+export async function fetchThreads(pageNum = 1, pageSize = 20) {
+  connectToDB();
+  const skipAmount = (pageNum - 1) * pageSize; //pagenum=1 means no skip i.e we are on first page viewing the first 20 threads
+
+  //   we will find top level thread that are not comments
+  const postsQuery = Thread.find({
+    parentId: { $in: [null, undefined] },
+  })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({
+      path: "author",
+      model: User,
+    })
+    // .populate({
+    //   path: "community",
+    //   model: Community,
+    // })
+    .populate({
+      path: "children", // Populate the children field
+      populate: {
+        path: "author", // Populate the author field within children
+        model: User,
+        select: "_id name parentId image", // Select only _id and username fields of the author
+      },
+    });
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  }); // Get the total count of posts
+
+  const posts = await postsQuery.exec();
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
 }
