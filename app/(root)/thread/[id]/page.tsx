9a28e0583a7a2ef6c8bdf39d9f044863dfbb30e8
Abdullah Aziz <abdullahazees4@gmail.com>
2023-09-11 21:20:57 +0500

    files added

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -33,9 +33,25 @@ export default async function Page({ params }: { params: { id: string } }) {
       <div className="mt-7">
         <Comment
           threadId={post._id}
-          currentUserImage={user?.imageUrl}
+          currentUserImage={userInfo.image}
           currentUserId={JSON.stringify(userInfo?._id) || ""}
         />
+      </div>
+      <div className="mt-10">
+        {post.children.map((childItem: any) => (
+          <ThreadCard
+            key={childItem._id}
+            id={childItem._id}
+            currentUserId={user.id}
+            parentId={childItem.parentId}
+            content={childItem.text}
+            author={childItem.author}
+            community={childItem.community}
+            createdAt={childItem.createdAt}
+            comments={childItem.children}
+            isComment
+          />
+        ))}
       </div>
     </section>
   );
