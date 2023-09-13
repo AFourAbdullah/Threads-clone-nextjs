8216d79023c4e3153deb8d34e85ae57d7dbf761a
Abdullah Aziz <abdullahazees4@gmail.com>
2023-09-13 23:55:34 +0500

    code added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,15 @@
 import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchThreads } from "@/lib/actions/threads.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { UserButton, currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const result = await fetchThreads(1, 30);
   const user = await currentUser();
-  // if (!user) return null;
+  if (!user) return null;
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
   console.log("threadss:", result);
   return (
     <>
